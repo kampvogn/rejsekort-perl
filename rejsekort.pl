4bca41e88e90dfd6471ddf4537b035065abfe586
Thomas Larsen <thomas@kampvogn.dk>
2013-04-15 21:07:40 +0200

    Added commandline switches for username and password, invoke with ./rejsekort.pl -u username -p password

--- rejsekort.pl
+++ rejsekort.pl
@@ -2,10 +2,22 @@
 use strict;
 use warnings;
 
+use Getopt::Std;
 use TravelCard::Selfcare;
 
-my $username = 'username'; ## Your username for https://selvbetjening.rejsekort.dk/CommercialWebSite  
-my $password = 's3cret';   ## Your password
+my $username = ''; ## Your username for https://selvbetjening.rejsekort.dk/CommercialWebSite  
+my $password = ''; ## Your password
+
+my %opts;
+getopt('u:p:', \%opts);
+
+if (exists($opts{'u'}) && $username eq '') {
+  $username = $opts{'u'};
+}
+
+if (exists($opts{'p'}) && $password eq '') {
+  $password = $opts{'p'};
+}
 
 binmode( STDOUT, ':utf8');
 
